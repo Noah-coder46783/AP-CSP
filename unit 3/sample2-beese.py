6f2ac94ce5a7a960deff72d5c96d8a36d2d728e3
Noah-coder46783 <noahbeese28@easttroy.k12.wi.us>
2025-10-16 10:35:33 -0500

    Add files via upload

---
+++ unit 3/sample2-beese.py
@@ -0,0 +1,7 @@
+name = input("what is your name")
+print("hello, " + name + "!")
+age = int(input("how old are you?"))
+if age < 18:
+    print("you are a minor")
+else:
+    print("you are an adult")
